a4f7bfdc8d5b007191b0e2a43077cfd4e3f8b350
Ferdinand <holdenriederferdinand@gmail.com>
2021-12-14 17:52:37 +0100

    postman muss funktionieren um weiter zu machen

--- controllers/autos.js
+++ controllers/autos.js
@@ -1,5 +1,7 @@
+import res from "express/lib/response";
 import pool from "../db/pg.js"
 
+//Alle Autos aus Auto holen
 export const getAllAutos = (req, res) => {
      pool
         .query("select * from Auto")
@@ -7,10 +9,40 @@ export const getAllAutos = (req, res) => {
         .catch((err) => console.log(err));
 };
 
+//Einzelnes Auto aus Auto holen
 export const getSingleAuto = (req, res) => {
    const id = req.params.id;
     pool
          .query("select * from Auto WHERE id=$1", [id])
-         .then((data) => res.json(data.rows[0]))
+         .then((data) => {
+            if(data.rowCount === 0) {
+                res.status(404).send("Wir haben bald mehr Autos für Sie!")
+            } else {
+                res.status(200).json(data.rows[0]);
+            }
+        })
          .catch((err) => console.log(err));
+};
+
+//Zu Auto hinzufügen
+export const createAuto = (req, res) => {
+    console.log("Contrtoller wird aufgerufen");    
+};
+
+//Auto aus Auto Löschen
+export const deleteAuto = (req,res) => {
+    const id = req.params.id;
+    pool.query("DELETE FROM Auto WHERE id=$1", [id])
+    .then(data => {
+        if(data.rowCount === 0) {
+            res.status(404).send("Wir haben bald mehr Autos für Sie!")
+        } else {
+            res.status(200).send("Auto erfolgreich gelöscht!");
+        }
+    })
+    .catch((err) => res.status(500).json(err));
+};
+
+export const updateAuto = (req, res) => {
+    console.log("Hier soll ein Auto aktualisiert werden!")
 };
--- routes/autos.js
+++ routes/autos.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { getAllAutos, getSingleAuto } from "../controllers/autos.js"
+import { createAuto, deleteAuto, getAllAutos, getSingleAuto, updateAuto } from "../controllers/autos.js"
 const autos = express.Router();
 
 //Um alle Autos zu bekommen 
 autos.route("/").get(getAllAutos)
 
+//Um ein Auto hinzuzufügen
+autos.route("/").post(createAuto)
+
 //Um ein einzelnes Auto zu bekommen 
 autos.route("/:id").get(getSingleAuto)
 
+//Um ein Auto zu Löschen
+autos.route("/:id").delete(deleteAuto)
+
+//Um ein Auto zu Updaten
+autos.route("/:id").put(updateAuto)
 export default autos;
