fd12dac8f1875266b6525c03f40ffa7868c2a746
Ferdinand <holdenriederferdinand@gmail.com>
2021-12-15 13:03:25 +0100

    port gelöscht

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import autos from "./routes/autos.js";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
